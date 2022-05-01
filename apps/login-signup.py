2577ad93456bac6e4cdd1b2196fa6094e845e9bb
Ayush Kumar Gupta <67339899+imayush18@users.noreply.github.com>
2022-05-01 22:47:54 +0530

    Update login-signup.py

--- apps/login-signup.py
+++ apps/login-signup.py
@@ -18,3 +18,65 @@ def check_hashes(password,hashed_text):
 	if make_hashes(password) == hashed_text:
 		return hashed_text
 	return False
+# DB Management
+import sqlite3 
+conn = sqlite3.connect('vehemence-data.db',check_same_thread=False)
+c = conn.cursor()
+# DB  Functions
+def create_usertable():
+	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
+
+
+def add_userdata(username,password):
+	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+	conn.commit()
+
+def login_user(username,password):
+	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+	data = c.fetchall()
+	return data
+
+
+def view_all_users():
+	c.execute('SELECT * FROM userstable')
+	data = c.fetchall()
+	return data
+
+
+
+def main():
+	"""Vehemence Music App"""
+
+	st.title("Vehemence Music App")
+
+	menu = ["Home","Login","SignUp"]
+	choice = st.sidebar.selectbox("Menu",menu)
+
+	if choice == "Home":
+		st.subheader("Home")
+
+	elif choice == "Login":
+		st.subheader("Login Section")
+
+		username = st.sidebar.text_input("User Name")
+		password = st.sidebar.text_input("Password",type='password')
+		if st.sidebar.checkbox("Login"):
+			# if password == '12345':
+			create_usertable()
+			hashed_pswd = make_hashes(password)
+
+			result = login_user(username,check_hashes(password,hashed_pswd))
+			if result:
+
+				st.success("Logged In as {}".format(username))
+				
+				#task = st.selectbox("Task",["Play songs"])
+				task = "Play songs"
+				if task == "Play songs":
+				 	#st.subheader("Detecting Emotion")
+					#import streamlit as st
+					from multiapp import MultiApp
+					from . import music
+					app = MultiApp()
+					app.add_app("Emotion-Based", music.main)
+					app.add_app("Voice-Based", emma.assistant)
